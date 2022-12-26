62987f098c8e6de986717e7c7a20ad65a18ccd93
Cahaya Gilang <gilang@dev.netkromsolution.com>
2022-12-26 20:48:19 +0700

    vendor asset

--- app/Views/layout/page_layout.php
+++ app/Views/layout/page_layout.php
@@ -18,13 +18,13 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/bootstrap-icons/bootstrap-icons.css') ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/boxicons/css/boxicons.min.css') ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/quill/quill.snow.css') ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/quill/quill.bubble.css') ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/remixicon/remixicon.css') ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/simple-datatables/style.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendorAsset/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendorAsset/bootstrap-icons/bootstrap-icons.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendorAsset/boxicons/css/boxicons.min.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendorAsset/quill/quill.snow.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendorAsset/quill/quill.bubble.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendorAsset/remixicon/remixicon.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendorAsset/simple-datatables/style.css') ?>" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
@@ -63,14 +63,14 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="<?= base_url('assets/vendor/apexcharts/apexcharts.min.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/chart.js/chart.umd.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/echarts/echarts.min.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/quill/quill.min.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/simple-datatables/simple-datatables.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/tinymce/tinymce.min.js') ?>"></script>
-  <script src="<?= base_url('assets/vendor/php-email-form/validate.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/apexcharts/apexcharts.min.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/chart.js/chart.umd.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/echarts/echarts.min.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/quill/quill.min.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/simple-datatables/simple-datatables.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/tinymce/tinymce.min.js') ?>"></script>
+  <script src="<?= base_url('assets/vendorAsset/php-email-form/validate.js') ?>"></script>
 
   <!-- Template Main JS File -->
   <script src="<?= base_url('assets/js/main.js') ?>"></script>
